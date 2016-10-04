019946535fb0741485c5b13559f06a6c13aa29f5
Maciej <mac201@vp.pl>
2016-10-04 16:37:25 +0200

    package

--- 04_scope/main/main.go
+++ 04_scope/main/main.go
@@ -20,10 +20,50 @@ func main() {
 	fmt.Println()
 	fmt.Println(increment())
 	fmt.Println(increment())
-}
 
+	//anonymous function
+
+	inc := func() int {
+		x++
+		return x
+	}
+
+	fmt.Printf("\n anonymous function \n")
+	fmt.Println(inc())
+	fmt.Println(inc())
+
+	inc2 := wrapper()
+	fmt.Printf("\n Return function \n")
+	fmt.Println(inc2())
+	fmt.Println(inc2())
+
+}
 
 func increment() int {
 	x++
 	return x
 }
+
+func wrapper() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+
+}
+/*
+Types:
+Method sets
+Boolean types
+Numeric types
+String types
+Array types
+Slice types
+Struct types
+Pointer types
+Function types
+Interface types
+Map types
+Channel types
+*/
