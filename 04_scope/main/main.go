43c72af14413b8a9c99f4fdabf1d4d59a12d1b45
Maciej <mac201@vp.pl>
2016-10-04 16:14:14 +0200

    package

--- 04_scope/main/main.go
+++ 04_scope/main/main.go
@@ -3,8 +3,27 @@ package main
 import "fmt"
 import "github.com/dragon200M/GoTraining/04_scope/vis"
 
+var x int = 42
+
 func main() {
 	fmt.Println(vis.LastName)
 	vis.PrintVa()
+	fmt.Println()
+	{
+		fmt.Println(x)
+		y := 34
+		fmt.Println(y)
 
+	}
+	//fmt.Println(y) y niedostepne
+
+	fmt.Println()
+	fmt.Println(increment())
+	fmt.Println(increment())
+}
+
+
+func increment() int {
+	x++
+	return x
 }
