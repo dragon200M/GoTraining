d426833e2fd78ff4d901618df3c9f2e54e022b62
Maciej <mac201@vp.pl>
2016-10-18 13:18:30 +0200

    digital root

---
+++ 07_forLoop/01/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 0
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	for{
+		i++
+		if i%2 == 0{
+			fmt.Println(i)
+			continue
+
+		}
+
+		if i >=20 {
+			break
+		}
+
+
+	}
+	for i := 50; i <= 140; i++ {
+		fmt.Printf("%v - %v - %v \n", i, string(i), []byte(string(i)))
+
+	}
+
+}
