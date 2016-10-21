9f71fa2a3eafe7c01121cf52f878dccb5259949e
Maciej <mac201@vp.pl>
2016-10-21 10:16:17 +0200

    rune

---
+++ 08_rune/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+ 	fmt.Println("hello")
+	fmt.Println([]byte("Hello"))
+
+	for i:= 50; i<=140 ;i++{
+		fmt.Println(i, " - ",string(i)," - ",[]byte(string(i)))
+
+	}
+
+	for i:= 5000; i<=5100 ;i++{
+		fmt.Println(i, " - ",string(i)," - ",[]byte(string(i)))
+
+	}
+
+}
