968739246977a83d40284ad5dd6449118434f127
Maciej <mac201@vp.pl>
2016-10-26 21:02:09 +0200

    variadic

--- 10_function/03_variadic/main.go
+++ 10_function/03_variadic/main.go
@@ -11,9 +11,28 @@ func main() {
 	//2
 	fmt.Println("Total:", average(a...))
 
+	//3
+	b := []float64{2, 4, 5, 7}
+	fmt.Println("Total:", average2(b))
+	//4
+	fmt.Println("Total:", average2([]float64{2, 4, 5, 7}))
+
 }
 
 func average(n ...float64) float64 {
+
+	var total float64
+
+	for k, i := range n {
+		fmt.Print(k, ":", i, " ")
+		total += i
+	}
+
+	return total / float64(len(n))
+
+}
+
+func average2(n []float64) float64 {
 
 	var total float64
 
