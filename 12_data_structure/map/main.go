7025359f5a3f6c6d627515cf861ac3cf57c306ff
Maciej <mac201@vp.pl>
2016-11-10 19:32:02 +0100

    ang

--- 12_data_structure/map/main.go
+++ 12_data_structure/map/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	
+
+	m:=make(map[string]int)
+
+	m["k1"] = 1
+	m["k1"] = 2
+	m["k2"] = 2
+	delete(m,"k1")
+
+	v,k :=m["k2"]
+	fmt.Println(v,k)
+
+
+
+	m2 :=map[string]int{"foo":1,"bar":2}
+
+	v1,k1:=m2["foo"]
+	fmt.Println(v1,k1)
 }
