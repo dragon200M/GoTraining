81f337029ade6c68e8b7392a540e34d2e49b4a51
Maciej <mac201@vp.pl>
2016-12-05 15:14:15 +0100

    struct

--- 13_struct/01/main.go
+++ 13_struct/01/main.go
@@ -6,19 +6,26 @@ type person struct{
 	first string
 	last string
 	age int
-	adres addr
+	addr
+
+
 }
 type  addr struct {
 	country string
+	city
+}
+
+type city struct {
+	city string
 }
 
 func main() {
-	a := addr{"Poland"}
-	me := person{"Maciej","Michalik",27,addr{"poland"}}
+	a := addr{"Poland",city{"Cracow"}}
+	me := person{"Maciej","Michalik",27,addr{"poland",city{"Cracow"}}}
 	me2 := person{"Maciej","Michalik",27,a}
 	fmt.Println(me2)
 	fmt.Println(me)
-	fmt.Println(me.first,me.last,me.age,me.adres.country)
+	fmt.Println(me.first,me.last,me.age,me.country,me.city.city)
 
 
 }
