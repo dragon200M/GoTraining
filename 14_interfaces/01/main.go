595f60cccdefe94747b000783497afc2bfd6ac50
Maciej <mac201@vp.pl>
2016-12-08 12:11:17 +0100

    interface

--- 14_interfaces/01/main.go
+++ 14_interfaces/01/main.go
@@ -11,6 +11,10 @@ func (z Square) area() float64 {
 	return z.a * z.a
 }
 
+func (z Square) circuit() float64 {
+	return 4*z.a
+}
+
 type Traingle struct {
 	a float64
 	h float64
@@ -21,14 +25,21 @@ func (t Traingle) area() float64 {
 	return (t.a * t.h) / 2
 }
 
+func (t Traingle) circuit() float64 {
+	return 0
+}
+
 func info(s Shape) {
+	fmt.Printf("%T \n",s)
 	fmt.Println(s)
 	fmt.Println(s.area())
+	fmt.Println(s.circuit())
 
 }
 
 type Shape interface {
 	area() float64
+	circuit() float64
 }
 
 func main() {
