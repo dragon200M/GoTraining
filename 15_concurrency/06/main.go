026bfbbbe977db59543866aba411ef4ef01a5b58
Maciej <mac201@vp.pl>
2016-12-09 21:21:55 +0100

    channels

--- 15_concurrency/06/main.go
+++ 15_concurrency/06/main.go
@@ -3,27 +3,34 @@ package main
 import (
 	"fmt"
 	"time"
+	"strconv"
 )
 
 func main() {
 
 	c :=make(chan  int)
-
+	s :=make(chan string)
+	var zm int
 
 	go func(){
 		for i:=0;i<10;i++{
 			c<-i
+			
+			zm = i*i*i
+			s<-strconv.Itoa(zm)
+
 		}
 	}()
 
 	go func(){
 		for {
-			fmt.Println(<-c)
+			fmt.Println(<-c,<-s)
+
 		}
 	}()
 
 
-	time.Sleep(time.Second)
+	time.Sleep(3*time.Second)
 
 
 }
