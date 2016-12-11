8a949e5ea56fc017ec5cddeba23ab2692f30132c
Maciej <mac201@vp.pl>
2016-12-11 21:36:20 +0100

    pipline

---
+++ 15_concurrency/13/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	//pipline
+	zm := gen(2, 3, 4)
+
+	for n := range seq(zm) {
+		fmt.Println(n)
+	}
+
+}
+
+func gen(nums ...int) chan int {
+	out := make(chan int)
+
+	go func() {
+		for _, n := range nums {
+
+			out <- n
+		}
+		close(out)
+
+	}()
+
+	return out
+}
+
+func seq(c chan int) chan int {
+	out := make(chan int)
+
+	go func() {
+		for n := range c {
+
+			out <- n * n
+		}
+		close(out)
+
+	}()
+
+	return out
+}
