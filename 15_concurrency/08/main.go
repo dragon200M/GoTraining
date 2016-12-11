c8f7dc1aeb6b2b9c436d1ebb1ad556eee501ee91
Maciej <mac201@vp.pl>
2016-12-11 18:46:01 +0100

    channels

--- 15_concurrency/08/main.go
+++ 15_concurrency/08/main.go
@@ -7,7 +7,7 @@ func main() {
 	done :=make(chan bool)
 
 
-	for i:=0;i<10;i++{
+	for j:=0;j<10;j++{
 	go func() {
 		for i := 0; i < 10; i++ {
 			c<-i
---
+++ 15_concurrency/09/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	c1 := inc("Foo:")
+	c2 := inc("Bar:")
+	c3 := puller(c1)
+	c4 := puller(c2)
+
+	fmt.Println(<-c3 , <-c4)
+
+}
+func puller(in chan int) chan int {
+	out := make(chan int)
+	go func() {
+		var suma int
+		for s := range in {
+			suma += s
+
+		}
+
+		out <- suma
+
+		close(out)
+	}()
+	return out
+
+}
+func inc(s string) chan int {
+	out := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			out <- i
+			fmt.Println(s, i)
+
+		}
+
+		close(out)
+
+	}()
+	return out
+}
